7d93e991bfa725f9e177cbd977488f4bb3563c0a
rubylo718 <rubylo718@gmail.com>
2023-09-09 15:09:14 +0800

    fix: move BrowserRouter from index to inside app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 import { TodoPage, LoginPage, SignUpPage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="todos" element={<TodoPage />}></Route>
-        <Route path="signup" element={<SignUpPage />}></Route>
-        <Route path="login" element={<LoginPage />}></Route>
-        <Route path="*" element={<TodoPage />}></Route>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="todos" element={<TodoPage />}></Route>
+          <Route path="signup" element={<SignUpPage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
+          <Route path="*" element={<TodoPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
