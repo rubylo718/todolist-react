7cf571bbc92780fee1b7156294b3a9243ff38604
rubylo718 <rubylo718@gmail.com>
2023-09-13 23:37:54 +0800

    fix: add router basename in App.js, update node version in depoly script

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.scss';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 import { AuthProvider } from 'context/AuthContext';
 
+const basename = process.env.PUBLIC_URL
+
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <AuthProvider>
           <Routes>
             <Route path="todos" element={<TodoPage />}></Route>
